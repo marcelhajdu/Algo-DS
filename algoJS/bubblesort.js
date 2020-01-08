e9685a54598775b0a53aaa4ed17815eef6998e61
Marcel Hajdu <hajdumarcel@gmail.com>
2020-01-08 10:01:19 +0100

    commented bubblesort

--- algoJS/bubblesort.js
+++ algoJS/bubblesort.js
@@ -1,7 +1,57 @@
+//implementálunk egy for loop-ot amelyik az array végérol indul 
+  //implementálunk még egy for loopot, amelyik az array elejétol indul,és addig megy, amíg kissebb nem lesz az elozo segédváltozónál -1
+    //ha a arr második segédváltozója kissebb mint a arr 2. segédváltozoja +1, akkor kicseréljuk oket
+bubbleSort([8,1,2,3,4,5,6,7]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function bubbleSort(arr){
+  //implementálunk egy for loop-ot amelyik az array végérol indul 
   for(var i = arr.length; i > 0; i--){
+    //implementálunk még egy for loopot, amelyik az array elejétol indul,és addig megy, amíg kissebb nem lesz az elozo segédváltozónál -1
     for(var j = 0; j < i - 1; j++){
+      //ha a arr második segédváltozója kissebb mint a arr 2. segédváltozoja +1, akkor kicseréljuk oket
       if(arr[j] > arr[j+1]){
         var temp = arr[j];
         arr[j] = arr[j+1];
